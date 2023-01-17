966f95f871e0511fd0780804bbf8b6a4f7521d66
ludovic <lume419@gmail.com>
2023-01-17 14:13:49 +0100

    Create Contrat.java

---
+++ Contrat.java
@@ -0,0 +1,31 @@
+
+public class Contrat {
+	int noContrat;
+	float prixEffectif;
+	var dateEffectif;
+	
+	public int getNoContrat() {
+		return this.noContrat;
+	}
+
+	public void setNoContrat(int noContrat) {
+		this.noContrat = noContrat;
+	}
+	
+	public float getPrixEffectif() {
+		return this.prixEffectif;
+	}
+
+	public void setPrixEffectif(float prixEffectif) {
+		this.prixEffectif = prixEffectif;
+	}
+
+	public var getDateEffectif() {
+		return this.dateEffectif;
+	}
+
+	public void setDateEffectif(var dateEffectif) {
+		this.dateEffectif = dateEffectif;
+	}
+
+}
