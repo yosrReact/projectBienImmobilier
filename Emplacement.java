0aa82c46784f89c1adc00ab4b8467ebdad7dd68f
joam <jdognon@gmail.com>
2023-01-17 14:44:03 +0100

    classe emplacement

---
+++ Emplacement.java
@@ -0,0 +1,25 @@
+public class Emplacement {
+    public String getDescription(){
+        return description;
+    }
+
+    public int getNbr_Piece(){
+        return nbr_Piece;
+    }
+
+    public int getSuperficie(){
+        return superficie;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setNbr_Piece(int nbr_Piece) {
+        this.nbr_Piece = nbr_Piece;
+    }
+
+    public void setSuperficie(int superficie){
+        this.superficie = superficie;
+    }
+}
