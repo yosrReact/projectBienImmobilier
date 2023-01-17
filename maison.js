9560d0e4a90b6861abe522d86f1651a9de99d507
ahmed_merniz <amtelecom94@gmail.com>
2023-01-17 14:13:12 +0100

    class_maison

---
+++ maison.js
@@ -0,0 +1,11 @@
+
+public class maison {
+
+	public static void main(String[] args) {
+		System.out.println("Nb_chambres : 3");
+		System.out.println("avec une cuisine equipée");
+		System.out.println("deuxième ligneet un jardin");
+
+	}
+
+}
