98969f2a87eb722ec7a637db1362f035f9d453a5
Chloé Kassegne <chloekassegne@macbook-pro-de-chloe.home>
2023-01-17 14:15:15 +0100

    ajout proprietaire

---
+++ Proprietaire.java
@@ -0,0 +1,23 @@
+import javafx.beans.binding.When.NumberConditionBuilder;
+
+public class Proprietaire {
+    int nProp;
+
+        public int getNProp(){
+            return nProp;
+        }
+
+        public void setNProp(int nProp){
+            this.nProp = nProp;
+        }
+
+    int numtel;
+        public int getNProp(){
+            return numtel;
+        }
+
+        public void setNProp(int numtel){
+            this.numtel = numtel;
+        }
+
+}
