d96b1d59b6a3f49d1884c36b2be62d7347158355
Dhekra <dhekra.sahbani@gmail.com>
2023-01-17 14:08:32 +0100

    first commit

---
+++ Immobilier.java
@@ -0,0 +1,12 @@
+public class BienImmob {
+
+	String statut;
+    String Localisation_BI;
+    String Etat_bien;
+    double Prix_achat;
+    double loyer_mensuel;
+    double charge_mensuel;
+
+
+	
+}
